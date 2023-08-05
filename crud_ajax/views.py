6a02d034efd7196fc5686022bc125e822a99d213
JitendraKumar8896 <jk8752277@gmail.com>
2023-08-05 19:59:56 +0530

    initial commit

---
+++ crud_ajax/views.py
@@ -0,0 +1,66 @@
+
+from django.views.generic import ListView
+from django.shortcuts import render
+from .models import CrudUser
+from django.views.generic import View
+from django.http import  JsonResponse
+
+class CrudView(ListView):
+    model = CrudUser
+    template_name = 'crud_ajax\crud.html'
+    context_object_name = 'users'
+
+# create user 
+
+class CreateCrudUser(View):
+    def  get(self, request):
+        name1 = request.GET.get('name', None)
+        address1 = request.GET.get('address', None)
+        age1 = request.GET.get('age', None)
+
+        obj = CrudUser.objects.create(
+            name = name1,
+            address = address1,
+            age = age1
+        )
+
+        user = {'id':obj.id,'name':obj.name,'address':obj.address,'age':obj.age}
+
+        data = {
+            'user': user
+        }
+        return JsonResponse(data)
+
+# update user         
+
+class UpdateCrudUser(View):
+    def  get(self, request):
+        id1 = request.GET.get('id', None)
+        name1 = request.GET.get('name', None)
+        address1 = request.GET.get('address', None)
+        age1 = request.GET.get('age', None)
+
+        obj = CrudUser.objects.get(id=id1)
+        obj.name = name1
+        obj.address = address1
+        obj.age = age1
+        obj.save()
+
+        user = {'id':obj.id,'name':obj.name,'address':obj.address,'age':obj.age}
+
+        data = {
+            'user': user
+        }
+        return JsonResponse(data)        
+
+# delete user
+
+class DeleteCrudUser(View):
+    def  get(self, request):
+        id1 = request.GET.get('id', None)
+        CrudUser.objects.get(id=id1).delete()
+        data = {
+            'deleted': True
+        }
+        return JsonResponse(data)     
+           
